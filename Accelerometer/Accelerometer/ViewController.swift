d28291350c6ad2189f8636fdc523f96c785e5548
Yoshinori Shibahara <yoshinori.shibahara@gmail.com>
2020-09-14 15:34:30 +0900

    Accelerometer

---
+++ Accelerometer/Accelerometer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by Yoshinori Shibahara on 2020/09/14.
+//  Copyright © 2020 Yoshinori Shibahara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToViewController(segue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
