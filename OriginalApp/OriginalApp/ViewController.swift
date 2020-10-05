c45256d8a7df54e91b7bd7c58745c101bf353b68
Yoshinori Shibahara <yoshinori.shibahara@gmail.com>
2020-10-05 10:41:35 +0900

    OriginalApp

---
+++ OriginalApp/OriginalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OriginalApp
+//
+//  Created by Yoshinori Shibahara on 2020/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
